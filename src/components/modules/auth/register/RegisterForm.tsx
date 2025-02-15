e5193d8b00307f87dfede8785a48e5a1932c15b5
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-15 20:14:34 +0600

    input field add in register page

---
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+const RegisterForm = () => {
+  const form = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel />
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Register</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterForm;
