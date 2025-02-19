9607753cda15ffcb757ec4ef2578fcb324080b41
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-19 12:45:57 +0600

    image uploader with create shop reausleable form

---
+++ src/app/(WithCommonLayout)/create-shop/page.tsx
@@ -0,0 +1,12 @@
+import CreateShopForm from "@/components/modules/shop/create-shop/CreateShopForm";
+import React from "react";
+
+const CreateShope = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <CreateShopForm></CreateShopForm>
+    </div>
+  );
+};
+
+export default CreateShope;
