384affdd8116d7b1373656472dc88e1bd5565673
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-23 23:46:36 +0600

    cart ui added success

--- src/app/(WithCommonLayout)/cart/page.tsx
+++ src/app/(WithCommonLayout)/cart/page.tsx
@@ -1,5 +1,4 @@
-// import CartProducts from "@/components/modules/cart/CartProducts";
-// import Coupon from "@/components/modules/cart/Coupon";
+import CartProducts from "@/components/modules/cart/CartProducts";
 import Coupon from "@/components/modules/cart/Coupon";
 import ProductBanner from "@/components/modules/products/banner";
 import NMContainer from "@/components/ui/core/NMContainer";
@@ -9,7 +8,7 @@ const CartPage = () => {
     <NMContainer>
       <ProductBanner title="Cart Page" path="Home - Cart" />
       <div className="grid grid-cols-12 gap-8 my-5">
-        {/* <CartProducts /> */}
+        <CartProducts />
         <Coupon />
       </div>
     </NMContainer>
