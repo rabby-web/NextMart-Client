539d795e4af702c7f58f96d1cb91c3bfb29b8029
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-23 22:21:11 +0600

    count down add to flash section use

--- src/components/modules/home/FlashSale/index.tsx
+++ src/components/modules/home/FlashSale/index.tsx
@@ -4,7 +4,7 @@ import ProductCard from "@/components/ui/core/ProductCard";
 import { getFlashSaleProducts } from "@/services/FlashSale";
 import { IProduct } from "@/types";
 import Link from "next/link";
-// import CountDown from "./CountDown";
+import CountDown from "./CountDown";
 
 const FlashSale = async () => {
   const { data: products } = await getFlashSaleProducts();
@@ -15,7 +15,7 @@ const FlashSale = async () => {
         <div className="flex items-center justify-between ">
           <div className="flex items-center gap-8">
             <h2 className="text-3xl font-bold">Flash Sale</h2>
-            {/* <CountDown /> */}
+            <CountDown />
           </div>
 
           <Link href="/products">
