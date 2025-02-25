bb8aaf7361b841ef057a14fafe4eb6b54c2f0eed
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-25 08:50:21 +0600

    simple product add to cart

--- src/components/modules/cart/CartProducts.tsx
+++ src/components/modules/cart/CartProducts.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import emptyCart from "@/assets/empty-cart.png";
-import { getAllProducts } from "@/services/Product";
 import CartProductCard from "./CartProductCard";
 import { IProduct } from "@/types";
-export default async function CartProducts() {
-  const { data: products } = await getAllProducts();
+import { orderedProductsSelector } from "@/redux/features/cartSlice";
+import { useAppSelector } from "@/redux/hooks";
+
+export default function CartProducts() {
+  const products = useAppSelector(orderedProductsSelector);
 
   return (
     <div className="border-2 border-white bg-background brightness-105 rounded-md col-span-8 h-full row-span-3 p-10 space-y-5">
