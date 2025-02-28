26824bd174ec7cd0055dd8a8fdf361f1a739b10c
rabby-web <rabby.webdeveloper@gmail.com>
2025-02-28 17:48:14 +0600

    reafactore code

--- src/app/(WithDashboardLayout)/layout.tsx
+++ src/app/(WithDashboardLayout)/layout.tsx
@@ -1,13 +1,5 @@
 import { AppSidebar } from "@/components/modules/dashboard/sidebar/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
+
 import {
   SidebarInset,
   SidebarProvider,
